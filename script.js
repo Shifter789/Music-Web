e7c59c66106bc1c5064ef639bcbb88e6cbe9a4c2
Percy <percygandhi211@gmail.com>
2026-01-31 06:47:27 +0000

    Added Halloween webpage

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 document.getElementById("Button0").onclick = function () {
     location.href = "Christmas/Christmas.html";
 };
+
+document.getElementById("Button1").onclick = function () {
+    location.href = "Halloween/Halloween.html";
+};
+
+document.getElementById("Button2").onclick = function () {
+    location.href = "Christmas/Christmas.html";
+};
